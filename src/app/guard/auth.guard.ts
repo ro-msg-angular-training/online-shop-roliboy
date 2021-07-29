5df4dca4703b0b0a9be1d3ad42a8b30681396537
Nagy Roland <roliboy@protonmail.com>
2021-07-29 15:38:30 +0300

    remove state from auth guard and make canActivate return observable

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -6,31 +6,32 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { IUser } from '../model/user.model';
-import { selectUser } from '../store/selector/auth.selector';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { selectUser } from '../store/reducer/auth.reducer';
 import { IAppState } from '../store/state/app.state';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  user?: IUser;
+  constructor(private store: Store<IAppState>, private router: Router) {}
 
-  constructor(
-    private store: Store<IAppState>,
-    private router: Router
-  ) {
-    this.store.pipe(select(selectUser)).subscribe((user) => (this.user = user));
-  }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.user) {
-      if (route.data.role && !this.user.roles.includes(route.data.role)) {
-        this.router.navigate(['/']);
-        return false;
-      }
-      return true;
-    }
-
-    this.router.navigate(['login']);
-    return false;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.store.pipe(
+      select(selectUser),
+      map((user) => {
+        if (!user) {
+          this.router.navigate(['login']);
+          return false;
+        }
+        if (route.data.role && !user.roles.includes(route.data.role)) {
+          this.router.navigate(['/']);
+          return false;
+        }
+        return true;
+      })
+    );
   }
 }
